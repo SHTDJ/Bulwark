2b20e55ef6ee2c2f5ba0256bd1c3506582a35e02
SHTDJ <niklas.nieberding@yahoo.de>
2018-05-08 07:29:03 +0200

    Prerelease of Multisend for individual addresses
    This release is not functional yet

--- src/qt/multisenddialog.h
+++ src/qt/multisenddialog.h
@@ -9,22 +9,20 @@ class MultiSendDialog;
 }
 
 class WalletModel;
-class QLineEdit;
 class MultiSendDialog : public QDialog
 {
     Q_OBJECT
-    void updateCheckBoxes();
+    void updateStatus();
+	void updateCheckBoxes();
 
 public:
     explicit MultiSendDialog(QWidget* parent = 0);
     ~MultiSendDialog();
     void setModel(WalletModel* model);
-    void setAddress(const QString& address);
+	void setAddress(const QString& address);
     void setAddress(const QString& address, QLineEdit* addrEdit);
 private slots:
-    void on_viewButton_clicked();
-    void on_addButton_clicked();
-    void on_deleteButton_clicked();
+	void deleteFrame();
     void on_activateButton_clicked();
     void on_disableButton_clicked();
     void on_addressBookButton_clicked();
