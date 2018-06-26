bd779d059e9361d64852d0d5590dbfe2d4f0d190
SHTDJ <niklas.nieberding@yahoo.de>
2018-06-26 03:30:18 +0200

    Revert "pls pls pls work"
    This reverts commit 906af92d9cec4beeb44d99a82acdc419aa87a00b.

--- src/netbase.h
+++ src/netbase.h
@@ -90,7 +90,7 @@ public:
     bool GetIn6Addr(struct in6_addr* pipv6Addr) const;
 
     friend bool operator==(const CNetAddr& a, const CNetAddr& b);
-    friend bool operator!=(const CNetAddr& a, const CNetAddr& b) { return !(a == b); }
+    friend bool operator!=(const CNetAddr& a, const CNetAddr& b);
     friend bool operator<(const CNetAddr& a, const CNetAddr& b);
 
     ADD_SERIALIZE_METHODS;
@@ -98,7 +98,7 @@ public:
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion)
     {
-        READWRITE(ip);
+        READWRITE(FLATDATA(ip));
     }
 
     friend class CSubNet;
