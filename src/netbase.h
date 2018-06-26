8281691f5bce28546c113bc328a104c650b0dd92
SHTDJ <niklas.nieberding@yahoo.de>
2018-06-26 02:36:33 +0200

    debug 1203102301241

--- src/netbase.h
+++ src/netbase.h
@@ -128,7 +128,7 @@ public:
     friend bool operator<(const CSubNet& a, const CSubNet& b);
 	ADD_SERIALIZE_METHODS;
     template <typename Stream, typename Operation>
-	inline void SerializationOp(Stream& s, Operation ser_action) {
+	inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
 	READWRITE(network);
 	READWRITE(netmask);
 	READWRITE(valid);
